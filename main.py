facbc0a69b0e351cc6300f77a357aff24cd74b95
Jose Castro <55367139+castrojose@users.noreply.github.com>
2022-10-27 16:32:06 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,99 @@
+import rp2
+import network
+import ubinascii
+import machine
+import urequests as requests
+import time
+from secrets import secrets
+import socket
+
+
+rp2.country('DE')
+
+wlan = network.WLAN(network.STA_IF)
+wlan.active(True)
+
+mac = ubinascii.hexlify(network.WLAN().config('mac'),':').decode()
+print('mac = ' + mac)
+
+
+ssid = secrets['picow']
+pw = secrets['tec123tec123']
+
+wlan.connect(ssid, pw)
+
+
+timeout = 10
+while timeout > 0:
+    if wlan.status() < 0 or wlan.status() >= 3:
+        break
+    timeout -= 1
+    print('Waiting for connection...')
+    time.sleep(1)
+
+  
+def blink_onboard_led(num_blinks):
+    led = machine.Pin('LED', machine.Pin.OUT)
+    for i in range(num_blinks):
+        led.on()
+        time.sleep(.2)
+        led.off()
+        time.sleep(.2)
+    
+
+
+wlan_status = wlan.status()
+blink_onboard_led(wlan_status)
+
+if wlan_status != 3:
+    raise RuntimeError('Wi-Fi connection failed')
+else:
+    print('Connected')
+    status = wlan.ifconfig()
+    print('ip = ' + status[0])
+    
+   
+def get_html(html_name):
+    with open(html_name, 'r') as file:
+        html = file.read()
+        
+    return html
+
+addr = socket.getaddrinfo('0.0.0.0', 80)[0][-1]
+
+s = socket.socket()
+s.bind(addr)
+s.listen(1)
+
+print('Listening on', addr)
+led = machine.Pin('LED', machine.Pin.OUT)
+
+
+while True:
+    try:
+        cl, addr = s.accept()
+        print('Client connected from', addr)
+        r = cl.recv(1024)
+        # print(r)
+        
+        r = str(r)
+        led_on = r.find('?led=on')
+        led_off = r.find('?led=off')
+        print('led_on = ', led_on)
+        print('led_off = ', led_off)
+        if led_on > -1:
+            print('LED ON')
+            led.value(1)
+            
+        if led_off > -1:
+            print('LED OFF')
+            led.value(0)
+            
+        response = get_html('index.html')
+        cl.send('HTTP/1.0 200 OK\r\nContent-type: text/html\r\n\r\n')
+        cl.send(response)
+        cl.close()
+        
+    except OSError as e:
+        cl.close()
+        print('Connection closed')
